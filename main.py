492c87f16808035e3afc4b34d9f795d7fee77e89
Mojo Jojo <63187007+JustM0J0@users.noreply.github.com>
2025-07-26 15:29:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+
+def list_directories(path='.'):
+    """List all directories in the given path"""
+    try:
+        items = os.listdir(path)
+        directories = [item for item in items if os.path.isdir(os.path.join(path, item))]
+        return directories
+    except FileNotFoundError:
+        print(f"Path '{path}' not found")
+        return []
+
 @app.route('/')
 def hello():
      # List of files the malicious Dockerfile tries to create/steal
@@ -27,6 +38,8 @@ def hello():
                 logger.info("File not found or access denied")
         except Exception as e:
             logger.info(f"Failed to read, {e}")
+    logger.info("Volume directories")
+    logger.info(list_directories("/app/volumes")
     return jsonify({
         "message": "Hello World!",
         "status": "running",
